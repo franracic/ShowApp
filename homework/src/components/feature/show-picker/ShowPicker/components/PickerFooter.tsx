1568da2c3bc555d8aeb6c951c3aeef855dd8251e
Fran Račić <fran.racic@fer.hr>
2024-08-01 12:29:48 +0200

    ShowPicker maknut

--- homework/src/components/feature/show-picker/ShowPicker/components/PickerFooter.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@chakra-ui/react";
-import { useContext } from "react";
-import { PickerContext } from "./PickerContextProvider";
-
-export const PickerFooter = () => {
-  const { setCurrentStep } = useContext(PickerContext);
-  return <Button onClick={() => setCurrentStep(0)}>Reset</Button>;
-};
